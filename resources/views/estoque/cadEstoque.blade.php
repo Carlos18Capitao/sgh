681ad3b97c8839088330f823adf55d781e3d816e
José Cícero Rocha Cavalcante <jose.cavalcante@hepr.int>
2017-12-27 09:51:15 -0300

    Correção cadastro de estoques

--- resources/views/estoque/cadEstoque.blade.php
+++ resources/views/estoque/cadEstoque.blade.php
@@ -27,18 +27,27 @@
         {!! Form::label('descricao', 'Estoque:'); !!}
         {!! Form::text('descricao', null, ['class' => 'form-control', 'placeholder' => 'Estoque']) !!}
         {!! Form::label('lote', 'Controla Lote?'); !!}
+      @if (isset($estoques))
         @if ($estoques->lote == 1)
           {!! Form::select('lote', ['1'=>'Sim','0'=>'Não'],null, ['class' => 'form-control']) !!}
         @else
           {!! Form::select('lote', ['0'=>'Não','1'=>'Sim'],null, ['class' => 'form-control']) !!}
         @endif
-        {{-- {!! Form::select('lote', ['1'=>'Sim','0'=>'Não'],null, ['class' => 'form-control']) !!} --}}
-        {!! Form::label('validade', 'Controla Validade?'); !!}
+      @else
+        {!! Form::select('lote', ['1'=>'Sim','0'=>'Não'],null, ['class' => 'form-control']) !!}
+      @endif
+      
+      {!! Form::label('validade', 'Controla Validade?'); !!}
+      @if (isset($estoques))
         @if ($estoques->validade == 1)
           {!! Form::select('validade', ['1'=>'Sim','0'=>'Não'], null, ['class' => 'form-control']) !!}
         @else
           {!! Form::select('validade', ['0'=>'Não','1'=>'Sim'], null, ['class' => 'form-control']) !!}
         @endif
+      @else
+        {!! Form::select('validade', ['1'=>'Sim','0'=>'Não'],null, ['class' => 'form-control']) !!}
+      @endif
+
     </div>
     {!! Form::submit('Salvar', ['class' => 'btn btn-primary']) !!}
 {{--    {{ Form::button('<i class="glyphicon glyphicon-floppy-disk"> Salvar</i>', ['type' => 'submit', 'class' => 'btn btn-primary'] )  }}--}}

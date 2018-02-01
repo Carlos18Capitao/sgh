88f6af9805b65794e082daeb8ec7b0f60e2bfd77
jcicero@gmail.com <csw2ylx9>
2018-01-31 21:12:58 -0300

    Ajuste de tamanho da fonte no menu de seleção do estoque

--- resources/views/estoque/selectEstoque.blade.php
+++ resources/views/estoque/selectEstoque.blade.php
@@ -30,12 +30,12 @@
                         	           <div class="panel-heading">
                         	               <div class="row">
                         	                   <div class="col-xs-3">
-                        	                       <i class="fa fa-fw fa-cart-arrow-down fa-4x"></i>
+                        	                       <i class="fa fa-fw fa-cart-arrow-down fa-4x"> </i>
                                                  {{-- <b>{{ $estoque->descricao }}</b> --}}
                         	                   </div>
                         	                   <div class="col-xs-9 text-left">
-                        	                       <div style="font-size: 30px;">
-                                                   {{ $estoque->descricao }}
+                        	                       <div align="center" style="font-size: 20px;">
+                                                       {{ $estoque->descricao }}
 
                         	                       {{-- <i class="fa fa-fw fa-lg fa-exclamation-circle" style="margin:6px"></i> --}}
                         	                       </div>

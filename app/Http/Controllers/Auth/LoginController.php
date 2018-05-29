26df0c8e51cfcfa36c793ca718a8cb3e8d3d64d6
jcicero@gmail.com <csw2ylx9>
2018-05-29 09:17:19 -0300

    Data e hora acesso usuario

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
-use Carbon\Carbon;
-use App\User;
 
 class LoginController extends Controller
 {

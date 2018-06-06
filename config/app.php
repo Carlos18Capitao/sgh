3ba1762e4aaceaada99f6cfdcb33c4db7a5b0791
José Cícero Rocha Cavalcante <jcicero@gmail.com>
2018-06-06 17:45:04 -0300

    Traduzindo dataTables

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'SGA'),
+    'name' => env('APP_NAME', 'SGA - Sistema de Gestão de Almoxarifado'),
 
     /*
     |--------------------------------------------------------------------------

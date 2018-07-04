0bbd981ae0c1a248879a717a5354cb132cc8d803
jcicero@gmail.com <csw2ylx9>
2018-07-04 10:20:15 -0300

    Desativar item_empenho_id na entrada

--- database/migrations/2018_05_25_084839_add_item_empenho_id_produto_entrada.php
+++ database/migrations/2018_05_25_084839_add_item_empenho_id_produto_entrada.php
@@ -12,11 +12,11 @@ class AddItemEmpenhoIdProdutoEntrada extends Migration
      * @return void
      */
     public function up()
-    {
+    {/*
         Schema::table('produto_entradas', function ($table) {
             $table->integer('item_empenho_id')->unsigned()->nullable();
             $table->foreign('item_empenho_id')->references('id')->on('item_empenhos')->onDelete('cascade');
-        });
+        });*/
     }
 
     /**
